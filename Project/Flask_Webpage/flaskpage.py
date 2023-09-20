5704e07d863037c578fa579f7a6d7586ec6b32e5
Andrew Chen <superandrewchen11@gmail.com>
2023-09-19 23:47:20 -0500

    Basic Navbar and CSS
    Navbar and CSS code obtained and refactored for our purposes from Corey Schafer Flask Tutorial Part 2

--- Project/Flask_Webpage/flaskpage.py
+++ Project/Flask_Webpage/flaskpage.py
@@ -1,20 +1,27 @@
 # Imports
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 
 # Initialize Flask Application
 app = Flask(__name__)
 
 
+# Flask Route Definitions
 @app.route("/") # Decorator for route / (the home page)
 @app.route("/index") # Additional decorator for index route
 @app.route("/index.html") # Additional decorator for index.html route
 def hello():
-    return render_template('index.html');
+    return render_template('index.html')
+
+@app.route("/howitworks")
+@app.route("/howitworks.html")
+@app.route("/how_it_works")
+def howitworks():
+    return render_template('howitworks.html')
 
 @app.route("/application")
 def application():
-    return "Not Implemented"
+    return render_template('application.html')
 
 
 # Automatically runs debug mode if script is run directly
