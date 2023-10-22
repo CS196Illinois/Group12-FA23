a859bbaa7c124a2609462b2eea3e0b262759448a
Andrew Chen <superandrewchen11@gmail.com>
2023-10-21 20:53:02 -0500

    Preliminary Scikit Learn code put in
    First tutorial completed.
    Dependencies figured out.

--- Project/Scikit/model.py
+++ Project/Scikit/model.py
@@ -1,5 +1,4 @@
 # Import Statements
-from pydataset import data
 import pandas as pd
 import numpy as np
 from sklearn.linear_model import LogisticRegression
@@ -7,4 +6,21 @@ from sklearn.model_selection import train_test_split
 # We will want to use k-fold for the final model optimally instead of train_test_split
 
 
+# Global Variables
+data_location = 'data.csv'
+game_dataframe = pd.read_csv(data_location)
+game_dataframe = game_dataframe.drop(columns=['date', 'Team A', 'TeamB', 'A_minus_B'])
 
+game_dataframe = pd.get_dummies(game_dataframe, drop_first=True)
+
+# Test Train Split
+X_train, X_test, y_train, y_test = train_test_split(
+    game_dataframe.drop(columns=['A_result_Tie', 'A_result_Win']), 
+    game_dataframe['A_result_Win']
+)
+
+# Train the model using the training data
+LogReg = LogisticRegression(solver='lbfgs')
+LogReg.fit(X_train, y_train)
+
+print(LogReg.predict(X_test))
