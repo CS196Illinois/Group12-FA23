a618551022db6c3b02037e2fe2d324d7ff5b4a50
Andrew Chen <superandrewchen11@gmail.com>
2023-10-21 19:58:58 -0500

    Created .venv and import statements

---
+++ Project/Scikit/model.py
@@ -0,0 +1,10 @@
+# Import Statements
+from pydataset import data
+import pandas as pd
+import numpy as np
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split 
+# We will want to use k-fold for the final model optimally instead of train_test_split
+
+
+
