ccb60f0ab7983b03ffbeb326d2b0811c41f1470e
Andrew Chen <superandrewchen11@gmail.com>
2023-11-12 20:50:16 -0600

    Widget Starter Created

--- Project/Flask_Webpage/flaskpage.py
+++ Project/Flask_Webpage/flaskpage.py
@@ -5,13 +5,50 @@ from flask import Flask, render_template, url_for
 # Initialize Flask Application
 app = Flask(__name__)
 
+# Recent Calculations
+TIE_IMAGE = "static/NoLoss.png"
+HOME_WIN_IMAGE = "static/RightLoss.png"
+AWAY_WIN_IMAGE = "static/LeftLoss.png"
+JERSEY_ONE = "static/jerseyone.png"
+JERSEY_TWO = "static/jerseytwo.png"
+
+calculations = [
+    {
+        'year': 'YYYY',
+        'home': 'HOME',
+        'away': 'AWAY',
+        'percentages': [50, 50],
+        'home_jersey': JERSEY_ONE,
+        'away_jersey': JERSEY_TWO,
+        'background': TIE_IMAGE
+    },
+    {
+        'year': 'YYYY',
+        'home': 'HOME',
+        'away': 'AWAY',
+        'percentages': [70, 30],
+        'home_jersey': JERSEY_ONE,
+        'away_jersey': JERSEY_TWO,
+        'background': HOME_WIN_IMAGE
+    },
+    {
+        'year': 'YYYY',
+        'home': 'HOME',
+        'away': 'AWAY',
+        'percentages': [40, 50],
+        'home_jersey': JERSEY_ONE,
+        'away_jersey': JERSEY_TWO,
+        'background': AWAY_WIN_IMAGE
+    }
+]
+
 
 # Flask Route Definitions
 @app.route("/") # Decorator for route / (the home page)
 @app.route("/index") # Additional decorator for index route
 @app.route("/index.html") # Additional decorator for index.html route
 def hello():
-    return render_template('index.html')
+    return render_template('index.html', calcs=calculations)
 
 @app.route("/howitworks")
 @app.route("/howitworks.html")
