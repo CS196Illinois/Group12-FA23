bd2798a75676312a897a3ac71be1c5dade3e0a9a
Andrew Chen <superandrewchen11@gmail.com>
2023-12-02 16:13:12 -0600

    Application Integration Completed

--- Project/Flask_Webpage/flaskpage.py
+++ Project/Flask_Webpage/flaskpage.py
@@ -11,8 +11,8 @@ app = Flask(__name__)
 TIE_IMAGE = "static/NoLoss.png"
 HOME_WIN_IMAGE = "static/RightLoss.png"
 AWAY_WIN_IMAGE = "static/LeftLoss.png"
-JERSEY_ONE = "static/jerseyone.png"
-JERSEY_TWO = "static/jerseytwo.png"
+JERSEY_ONE = "static/flags/England.png"
+JERSEY_TWO = "static/flags/New_Zealand.png"
 
 calculations = [
     {
@@ -64,6 +64,32 @@ def howitworks():
 @app.route("/application")
 def application():
     return render_template('application.html')
+
+@app.route("/application/<home>/<away>/<year>/<field>")
+def submit(home, away, year, field):
+    percentages = model.LogReg.predict_proba([database.stats(int(year), home, away, field == "HOME")])[0]
+    percentages[0] = round(percentages[0] * 100, 1)
+    percentages[1] = round(percentages[1] * 100, 1)
+
+    background = "/static/NoLoss.png"
+    if (percentages[1] - percentages[0] >= 5):
+        background = "/static/RightLoss.png"
+    elif (percentages[0] - percentages[1] >= 5):
+        background = "/static/LeftLoss.png"
+
+    home_jersey = "/static/flags/" + "_".join(home.split(" ")) + ".png"
+    away_jersey = "/static/flags/" + "_".join(away.split(" ")) + ".png"
+
+    return render_template('application.html', params={
+        'home' : home,
+        'away' : away,
+        'year' : year,
+        'field' : field + " FIELD",
+        'percentages' : percentages,
+        'background' : background,
+        'home_jersey' : home_jersey,
+        'away_jersey' : away_jersey
+    })
 
 
 # Automatically runs debug mode if script is run directly
